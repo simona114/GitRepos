11ea13ea4b3dcef4a2bddaf855fec28b30d3c29f
Sissy <sisinikolova48@gmail.com>
2022-11-28 22:23:11 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/gitreposF101321/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.gitreposF101321
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gitreposF101321.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
