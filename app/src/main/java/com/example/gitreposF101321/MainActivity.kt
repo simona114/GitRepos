c35dc8c04df09258f1210cd084fd7c1ceb4a3731
Sissy <sisinikolova48@gmail.com>
2022-12-12 17:29:52 +0200

    Set up the navigation component. Add a start destination fragment

--- app/src/main/java/com/example/gitreposF101321/MainActivity.kt
+++ app/src/main/java/com/example/gitreposF101321/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.gitreposF101321
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.gitreposF101321.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,5 +17,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        //Enable navigation via back arrow on phone
+        navController.enableOnBackPressed(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp() || super.onSupportNavigateUp()
     }
 }
