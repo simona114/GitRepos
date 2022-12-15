1dd4e97682f2ef66b11ebd090140b2ccd88bf832
Sissy <sisinikolova48@gmail.com>
2022-12-15 16:30:28 +0200

    Add the missing file

---
+++ app/src/main/java/com/example/gitreposF101321/repos/data/ReposRepository.kt
@@ -0,0 +1,9 @@
+package com.example.gitreposF101321.repos.data
+
+import com.example.gitreposF101321.repos.data.remote.RepositoriesRemoteDataSource
+
+class ReposRepository(private val remoteDataSource: RepositoriesRemoteDataSource) {
+
+    suspend fun getNewRepos() = remoteDataSource.getAllRepositories()
+
+}
