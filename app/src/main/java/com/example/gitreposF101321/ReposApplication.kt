8e98452113d33f11ee8e75523ff7fd7a7b3c2a1f
Simona Nikolova <57250076+simona114@users.noreply.github.com>
2022-12-23 16:47:10 +0200

    Fix imports

--- app/src/main/java/com/example/gitreposF101321/ReposApplication.kt
+++ app/src/main/java/com/example/gitreposF101321/ReposApplication.kt
@@ -1,6 +1,7 @@
 package com.example.gitreposF101321
 
 import android.app.Application
+import com.example.gitreposF101321.di.di
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
