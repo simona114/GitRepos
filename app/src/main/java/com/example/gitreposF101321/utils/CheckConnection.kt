b18daa75d94a54fa3a6bffd797260af8af15b184
Simona Nikolova <57250076+simona114@users.noreply.github.com>
2023-02-09 19:36:44 +0200

    Monitor network connection

---
+++ app/src/main/java/com/example/gitreposF101321/utils/CheckConnection.kt
@@ -0,0 +1,48 @@
+package com.example.gitreposF101321.utils
+
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkRequest
+import androidx.lifecycle.LiveData
+
+class CheckConnection(private val connectivityManager: ConnectivityManager) : LiveData<Boolean>() {
+    constructor(application: Application) : this(
+        application.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    )
+
+    init {
+        postValue(false)
+    }
+
+    private val networkCallback = createNetworkCallBack()
+
+    //is being observed
+    override fun onActive() {
+        super.onActive()
+
+        val request = NetworkRequest.Builder()
+        connectivityManager.registerNetworkCallback(request.build(), networkCallback)
+    }
+
+    //is not being observed
+    override fun onInactive() {
+        super.onInactive()
+        connectivityManager.unregisterNetworkCallback(networkCallback)
+
+    }
+
+    private fun createNetworkCallBack() = object : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+            postValue(true)
+        }
+
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            postValue(false)
+        }
+
+    }
+}
