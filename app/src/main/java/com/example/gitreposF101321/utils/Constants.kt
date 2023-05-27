dd011f9a221f5529212f42e3c7d9d91a5588faaa
Simona Nikolova <57250076+simona114@users.noreply.github.com>
2023-05-27 21:08:33 +0300

    Delete SQLite related code

--- app/src/main/java/com/example/gitreposF101321/utils/Constants.kt
+++ app/src/main/java/com/example/gitreposF101321/utils/Constants.kt
@@ -3,11 +3,5 @@ package com.example.gitreposF101321.utils
 class Constants {
     companion object {
         const val USER_NAME = "simona114"
-
-        const val COLUMN_INDEX_REPO_ID = 0
-        const val COLUMN_INDEX_REPO_TITLE = 1
-        const val COLUMN_INDEX_REPO_OWNER = 2
-        const val COLUMN_INDEX_REPO_LANGUAGE = 3
-        const val COLUMN_INDEX_REPO_LINK = 4
     }
 }
